72946c869af37812e1e47dced5d485804da1daab
aleramirezsj <aleramirezsj@gmail.com>
2024-05-13 15:25:24 -0300

    Inicio de proyecto

---
+++ Pages/35aniversario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MesasExamenes.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Proyecto ASP.NET  Mesas de Examenes 
+## Cátedra: Bases de datos II y Gestión de Proyectos de Software
+### 3er año - Técnico Superio en Desarrollo de Software
+***Año 2024***
+
+*Alejandro Ramirez*
